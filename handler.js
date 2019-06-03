7d4791a6b52c528127e16e2e8ad656d797908d8a
carlosshaka <carlos.shaka02@gmail.com>
2019-06-03 18:08:48 -0300

    :rocket: Change configuration to deploy on aws

--- handler.js
+++ handler.js
@@ -2,9 +2,9 @@
 const jwt = require('jsonwebtoken');
 
 module.exports.verifyJWTToken = async token => {
-  const { JWT_SECRET } = process.env
+  const { secret } = process.env
   try {
-    var decoded = jwt.verify(token, JWT_SECRET);
+    var decoded = jwt.verify(token, secret);
     const response = {
       statusCode: 200,
       body: decoded.foo
